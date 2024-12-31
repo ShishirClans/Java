43985b4c26cf2afae6b9e8c3214bc3342afe5d33
Shishir Ghorashainee <155758955+ShishirClans@users.noreply.github.com>
2024-12-31 18:21:12 -0500

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,10 @@
+public class CircleArea { 
+        public static void main(string[] args){
+        double radius;  //float radius
+        double area;
+        radius= 20.5;   // 20.5f;
+        final double PI = 3.142;
+        area = PI*radius*radius;
+        System.out.println("Area of Circle is: "+area);
+        }
+}
